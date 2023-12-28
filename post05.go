97d1ec1f49db1883903cfd2e8e27689e1b99d3f9
shiou61 <shiou278@gmail.com>
2023-12-28 10:56:53 +0900

    b

--- post05.go
+++ post05.go
@@ -44,27 +44,23 @@ func openConnection() (*sql.DB, error) {
 // -1 if the user does not exists
 func exists(username string) int {
 	username = strings.ToLower(username)
-
-	db, err := openConnection()
-	if err != nil {
-		fmt.Println(err)
-		return -1
-	}
-	defer db.Close()
-
-	userID := -1
-	statement := fmt.Sprintf(`SELECT "id" FROM "users" where username = '%s'`, username)
-	rows, err := db.Query(statement)
+    db, err := openConnection()
+    if err != nil {
+        fmt.Println(err)
+        return -1
+    }
+    defer db.Close()
+    userID := -1
+    statement := fmt.Sprintf(`SELECT "id" FROM "users" where username = '%s'`, username)
+    rows, err := db.Query(statement)
 
 	for rows.Next() {
-		var id int
-		err = rows.Scan(&id)
-		if err != nil {
-			fmt.Println("Scan", err)
-			return -1
-		}
-		userID = id
-	}
+        var id int
+        err = rows.Scan(&id)
+        if err != nil {
+            fmt.Println("Scan", err)
+            return -1
+        }
 	defer rows.Close()
 	return userID
 }
